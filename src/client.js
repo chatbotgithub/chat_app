a7799b17f5f5350de3652a1bcf16f70ca37ebf4b
chatbotgithub <chatbotgithub@gmail.com>
2024-06-19 00:31:53 +0500

    Update client.js

--- src/client.js
+++ src/client.js
@@ -1,5 +1,5 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://btqjthfcuitrwdaevkbg.supabase.co";
+const supabaseUrl = "https://sgfwciulmljuptqlouak.supabase.co";
 const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
 export const supabaseClient = createClient(supabaseUrl, supabaseKey);
